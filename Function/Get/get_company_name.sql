8661e34e88a9ae8e1c75be17d062df9cb2f8ae9b
ValMor561 <koli1babenko2@yandex.ru>
2023-06-23 23:46:37 +0300

    Замена типа столбца CompanyName на text

--- Function/Get/get_company_name.sql
+++ Function/Get/get_company_name.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION get_company_name()
-RETURNS TABLE (CompanyName varchar(20)) AS $$
+RETURNS TABLE (CompanyName text) AS $$
 BEGIN
   RETURN QUERY SELECT comp."Name" FROM "Company" comp;
 END;
--- Function/Get/get_employee.sql
+++ Function/Get/get_employee.sql
@@ -3,13 +3,13 @@ RETURNS TABLE (
 	employeetid integer,
     surname varchar(20),
     firsname varchar(20),
-	companyname varchar(20)
+	companyname text
 ) AS $$
 BEGIN
     RETURN QUERY
 		SELECT em."EmployeeID", em."Surname", em."Name", com."Name"
         FROM "Employee" em
         JOIN "Company" com ON em."CompanyId" = com."CompanyId"
-        ORDER BY em."EmployeeID" OFFSET (page_num - 1)* 55 LIMIT 55;
+        ORDER BY em."EmployeeID" OFFSET (page_num - 1)* 59 LIMIT 59;
 END;
 $$ LANGUAGE plpgsql;
