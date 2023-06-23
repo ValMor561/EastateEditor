010d62bb928aea0be755babc45181f52a178a007
ValMor561 <koli1babenko2@yandex.ru>
2023-06-23 23:48:03 +0300

    Сортировка по номеру паспорта при выводе данных

--- Function/Get/get_passport_client.sql
+++ Function/Get/get_passport_client.sql
@@ -14,6 +14,6 @@ BEGIN
     RETURN QUERY
 		SELECT ps."PassportNumber", ps."Surname", ps."Name", ps."PassportNumber", ps."CodeOffice", ps."Date", ps."City", ps."Age", ps."Family"
         FROM "PassportClient" ps
-        OFFSET (page_num - 1) * 59 LIMIT 59;
+        ORDER BY ps."PassportNumber" OFFSET (page_num - 1) * 59 LIMIT 59;
 END;
 $$ LANGUAGE plpgsql;
